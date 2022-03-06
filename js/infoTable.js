61e93a473ab98136424c0fa130e3529e53204f88
Juan Camilo Zorrilla <juanmiloz@hotmail.com>
2022-03-06 17:14:27 -0500

    add function to SPA

--- js/infoTable.js
+++ js/infoTable.js
@@ -2,12 +2,23 @@ $(document).ready(function() {
     $('#submitBtn').click(function() {
         var nStates = parseInt($('#nStates').val());
         var nInputs = parseInt($('#nInputs').val());
-        
+
         if(Number.isNaN(nStates) || Number.isNaN(nInputs)){
             $('#nStates').val('');
             $('#nInputs').val('');
         }else{
-
+            cambiarVista("tableView")
         }
     });
 });
+
+function cambiarVista(objetivo){
+    $(".view").hide();
+    $(".view").each(
+        function() {
+            if($(this).attr("id") == objetivo){
+                $(this).show();
+            }
+        }
+    );
+}
